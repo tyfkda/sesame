c8036fd32fcd879d387a4f691071ed4eef0158d5
tyfkda <tyfkda@gmail.com>
2020-12-15 08:43:08 +0900

    配列を扱えるようにする
    `while` ループで扱えることを増やせるようにするため、
    配列を追加する。
    構文じゃなくて関数呼び出しで対応する。
    
    * 組み込み関数追加
    * `puts` を追加
    * 自動的に最終結果を表示していたのをやめる

--- main.rb
+++ main.rb
@@ -1,12 +1,19 @@
 require './lib/compiler'
 require './lib/vm'
+require './runtime'
 
 def main()
-  global = {}
+  global = {
+    array_new: ->(n) { array_new(n) },
+    array_get: ->(array, i) { array_get(array, i) },
+    array_set: ->(array, i, value) { array_set(array, i, value) },
+    puts: ->(x) { puts(x) },
+  }
   compiler = Compiler.new(global, [])
   compiler.compile(ARGV[0])
 
   global.each do |k, v|
+    next if v.is_a?(Proc)
     puts "=== #{k} #{v[0].inspect}"
     v[1].dump()
     puts ""
@@ -14,8 +21,7 @@ def main()
   compiler.bbcon.dump()
 
   vm = VM.new(global, compiler.bbcon.bbs)
-  result = vm.run()
-  puts "result=#{result}"
+  vm.run()
 end
 
 if $0 == __FILE__
